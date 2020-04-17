d3c24238a23b3b51c1b2164f59475b90d5cf9b76
Johannes Jensen <spand@spand.org>
2020-04-17 15:11:39 +0200

    Initial commit.

---
+++ src/jvmMain/kotlin/sample/Foo.kt
@@ -0,0 +1,190 @@
+package sample
+
+import kotlinx.html.FlowContent
+import kotlinx.html.div
+
+class Foo {
+
+    fun FlowContent.foo() {
+        div {
+            +"dsad"
+            div {
+                +"Dasda"
+                div {
+                    +"Dasda"
+                    div {
+                        +"Dasda"
+                        div {
+                            +"Dasda"
+                            div {
+                                +"Dasda"
+                                div {
+                                    +"Dasda"
+                                    div {
+                                        +"Dasda"
+                                        div {
+                                            +"Dasda"
+                                            div {
+                                                +"Dasda"
+                                                div {
+                                                    +"Dasda"
+                                                    div {
+                                                        +"Dasda"
+                                                        div {
+                                                            +"Dasda"
+                                                            div {
+                                                                +"Dasda"
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            div {
+                +"Dasda"
+                div {
+                    +"Dasda"
+                    div {
+                        +"Dasda"
+                        div {
+                            +"Dasda"
+                            div {
+                                +"Dasda"
+                                div {
+                                    +"Dasda"
+                                    div {
+                                        +"Dasda"
+                                        div {
+                                            +"Dasda"
+                                            div {
+                                                +"Dasda"
+                                                div {
+                                                    +"Dasda"
+                                                    div {
+                                                        +"Dasda"
+                                                        div {
+                                                            +"Dasda"
+                                                            div {
+                                                                +"Dasda"
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+//    fun FlowContent.didntFinishInOneMinute() {
+//        div {
+//            +"dsad"
+//            div {
+//                +"Dasda"
+//                div {
+//                    +"ASdas"
+//                    div {
+//                        +"Asda"
+//                        div {
+//                            +"ASdasd"
+//                            div {
+//                                +"asdasd"
+//                                div {
+//                                    +"asdasda"
+//                                    div {
+//                                        +"Asdasda"
+//                                        div {
+//                                            +"Asd"
+//                                            div {
+//                                                +"ASdasdas"
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                                +"asdasd"
+//                                div {
+//                                    +"asdasda"
+//                                    div {
+//                                        +"Asdasda"
+//                                        div {
+//                                            +"Asd"
+//                                            div {
+//                                                +"ASdasdas"
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                                +"asdasd"
+//                                div {
+//                                    +"asdasda"
+//                                    div {
+//                                        +"Asdasda"
+//                                        div {
+//                                            +"Asd"
+//                                            div {
+//                                                +"ASdasdas"
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                                +"asdasd"
+//                                div {
+//                                    +"asdasda"
+//                                    div {
+//                                        +"Asdasda"
+//                                        div {
+//                                            +"Asd"
+//                                            div {
+//                                                +"ASdasdas"
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                                +"asdasd"
+//                                div {
+//                                    +"asdasda"
+//                                    div {
+//                                        +"Asdasda"
+//                                        div {
+//                                            +"Asd"
+//                                            div {
+//                                                +"ASdasdas"
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                                +"asdasd"
+//                                div {
+//                                    +"asdasda"
+//                                    div {
+//                                        +"Asdasda"
+//                                        div {
+//                                            +"Asd"
+//                                            div {
+//                                                +"ASdasdas"
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+}
