886e7eafcf35bef709a094f64a03237a44864006
Johannes Jensen <spand@spand.org>
2022-02-21 11:02:10 +0100

    Bump infra. Add libs instead of broken repos. Add more code to compile.

---
+++ src/jvmMain/kotlin/sample2/Foo2.kt
@@ -0,0 +1,190 @@
+package sample2
+
+import kotlinx.html.FlowContent
+import kotlinx.html.div
+
+private object Foo2 {
+
+    fun FlowContent.foo() {
+        div {
+            +"dsad2"
+            div {
+                +"Dasda"
+                div {
+                    +"Dasda"
+                    div {
+                        +"Dasda"
+                        div {
+                            +"Dasda"
+                            div {
+                                +"Dasda"
+                                div {
+                                    +"Dasda"
+                                    div {
+                                        +"Dasda"
+                                        div {
+                                            +"Dasda"
+                                            div {
+                                                +"Dasda"
+                                                div {
+                                                    +"Dasda"
+                                                    div {
+                                                        +"Dasda"
+                                                        div {
+                                                            +"Dasda"
+                                                            div {
+                                                                +"Dasda"
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            div {
+                +"Dasda"
+                div {
+                    +"Dasda"
+                    div {
+                        +"Dasda"
+                        div {
+                            +"Dasda"
+                            div {
+                                +"Dasda"
+                                div {
+                                    +"Dasda"
+                                    div {
+                                        +"Dasda"
+                                        div {
+                                            +"Dasda"
+                                            div {
+                                                +"Dasda"
+                                                div {
+                                                    +"Dasda"
+                                                    div {
+                                                        +"Dasda"
+                                                        div {
+                                                            +"Dasda"
+                                                            div {
+                                                                +"Dasda"
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun FlowContent.didntFinishInOneMinute() {
+        div {
+            +"dsad"
+            div {
+                +"Dasda"
+                div {
+                    +"ASdas"
+                    div {
+                        +"Asda"
+                        div {
+                            +"ASdasd"
+                            div {
+                                +"asdasd"
+                                div {
+                                    +"asdasda"
+                                    div {
+                                        +"Asdasda"
+                                        div {
+                                            +"Asd"
+                                            div {
+                                                +"ASdasdas"
+                                            }
+                                        }
+                                    }
+                                }
+                                +"asdasd"
+                                div {
+                                    +"asdasda"
+                                    div {
+                                        +"Asdasda"
+                                        div {
+                                            +"Asd"
+                                            div {
+                                                +"ASdasdas"
+                                            }
+                                        }
+                                    }
+                                }
+                                +"asdasd"
+                                div {
+                                    +"asdasda"
+                                    div {
+                                        +"Asdasda"
+                                        div {
+                                            +"Asd"
+                                            div {
+                                                +"ASdasdas"
+                                            }
+                                        }
+                                    }
+                                }
+                                +"asdasd"
+                                div {
+                                    +"asdasda"
+                                    div {
+                                        +"Asdasda"
+                                        div {
+                                            +"Asd"
+                                            div {
+                                                +"ASdasdas"
+                                            }
+                                        }
+                                    }
+                                }
+                                +"asdasd"
+                                div {
+                                    +"asdasda"
+                                    div {
+                                        +"Asdasda"
+                                        div {
+                                            +"Asd"
+                                            div {
+                                                +"ASdasdas"
+                                            }
+                                        }
+                                    }
+                                }
+                                +"asdasd"
+                                div {
+                                    +"asdasda"
+                                    div {
+                                        +"Asdasda"
+                                        div {
+                                            +"Asd"
+                                            div {
+                                                +"ASdasdas"
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
